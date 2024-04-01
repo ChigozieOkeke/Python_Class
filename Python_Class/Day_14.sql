d4be7e0a467c60279058224ddf94b3ee7774b65f
ChigozieOkeke <163838878+ChigozieOkeke@users.noreply.github.com>
2024-04-01 10:00:43 +0100

    Delete Python_Class directory

--- Python_Class/Day_14.sql
+++
@@ -1,5 +0,0 @@
-use employee_details;
-select * from studentsperformance;
-select * from studentsperformance where lunch like "________%";
-alter table studentsperformance add id integer not null first;
-alter table studentsperformance modify column id integer not null auto_increment primary key;
--- Python_Class/p.py
+++
@@ -1,6 +0,0 @@
-## string concatenation
-print("Hello" + "World")
-print("my name is" +" " + "world") ## string and strings 
-## string concatenation
-name="Agozie" 
-print("my name is" +" "+ name)## string and variables
